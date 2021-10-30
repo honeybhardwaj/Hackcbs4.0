80e47e38cf9957a9178a2122f6e71f1f63acf1e0
Anmol Jhamb <codingwithaj@gmail.com>
2021-10-30 13:05:34 +0530

    added the function to start recording when the time is near

--- app.py
+++ app.py
@@ -18,24 +18,44 @@ class Meeting(db.Model):
     class_name = db.Column(db.String(50))
     class_link = db.Column(db.String(50))
     class_time = db.Column(db.DateTime, primary_key=True)
+    started = db.Column(db.Boolean, default=False)
+    finished = db.Column(db.Boolean, default=False)
 
 
-def checker():
-    print(time.time())
-
-
-@app.route("/", methods=["GET", "POST"])
-def home():
+def get_meeting_list():
     meeting_list = []
     for meeting in Meeting.query.all():
         meeting_list.append(
             {
                 'class_name': meeting.class_name,
                 'class_link': meeting.class_link,
-                'class_time': meeting.class_time
+                'class_time': meeting.class_time,
+                'started': meeting.started,
+                'finished': meeting.finished
             }
         )
-    return render_template("index.html", meeting_list=meeting_list)
+    # Sort the list on the basis of the meeting times
+    meeting_list = sorted(meeting_list, key=lambda x: x['class_time'])
+    return meeting_list
+
+
+def start_recording(meeting):
+    # meeting.class_name 
+    # meeting.class_time
+    # meeting.class_link
+    print(f"Starting the recording of {meeting.class_name} at {meeting.class_time}")
+
+
+def checker():
+    for meeting in Meeting.query.all():
+        if datetime.now() > meeting.class_time and not meeting.started:
+            start_recording(meeting)
+            meeting.started = True 
+            db.session.commit()
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("index.html", meeting_list=get_meeting_list())
 
 
 @app.route('/add_meeting', methods=['POST'])
@@ -63,5 +83,5 @@ if __name__ == "__main__":
     db.create_all()
     scheduler.add_job(id="check_time", func=checker, trigger="interval", seconds=5)
     scheduler.start()
-    app.run(debug=True, port=5005)
+    app.run(debug=True, port=5001)
     
